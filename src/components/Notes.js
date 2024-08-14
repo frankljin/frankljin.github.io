607f0b103c1215138b8e21abd1dd3ef4ff2b2c48
Frank Jin <frankj@ziprecruiter.com>
2024-08-13 23:35:40 -0700

    final fix

--- src/components/Notes.js
+++ src/components/Notes.js
@@ -5,6 +5,7 @@ import CS245 from "../CS_245.pdf";
 import MATH138 from "../MATH_138.pdf";
 import MATH235 from "../MATH_235.pdf";
 import MATH237 from "../MATH_237.pdf";
+import MATH239 from "../MATH_239.pdf";
 import STAT231 from "../STAT_231.pdf";
 
 const Notes = () => {
@@ -23,12 +24,16 @@ const Notes = () => {
       <li>MATH 138 - Calculus 2: <Link to={MATH138} target="_blank">summary</Link></li>
       <li>MATH 235 - Linear Algebra 2: <Link to={MATH235} target="_blank">summary</Link></li>
       <li>MATH 237 - Calculus 3: <Link to={MATH237} target="_blank">summary</Link></li>
-      <li>STAT 231 - Statistics: <Link to={MATH237} target="_blank">course notes</Link></li>
+      <li>MATH 239 - Introduction to Combinatorics: <Link to={MATH239} target="_blank">summary</Link></li>
+      <li>STAT 231 - Statistics: <Link to={STAT231} target="_blank">course notes</Link></li>
+      <li>MH 3701 (NTU) - Basic Optimization: Coming Soon</li>
+      <li>CO 331 - Coding Theory: Coming Soon</li>
     </ul>
     <h3>Computer Science</h3>
     <ul>
         <li>CS 245 - Logic and Computation: <Link to={CS245} target="_blank">course notes</Link></li>
         <li>CS 370 - Numerical Computation: <Link to={CS370} target="_blank">course notes</Link></li>
+        <li>CZ 4041 (NTU) - Machine Learning: Coming Soon</li>
     </ul>
   </div>
 

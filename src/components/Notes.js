458e5cbb1759f62994a4d256dcf1966a38a3d121
Frank Jin <f7jin@uwaterloo.ca>
2025-03-09 23:01:36 -0400

    cahnges

--- src/components/Notes.js
+++ src/components/Notes.js
@@ -7,6 +7,7 @@ import MATH235 from "../MATH_235.pdf";
 import MATH237 from "../MATH_237.pdf";
 import MATH239 from "../MATH_239.pdf";
 import STAT231 from "../STAT_231.pdf";
+import PMATH347 from "../PMATH_347.pdf";
 
 const Notes = () => {
   
@@ -26,6 +27,7 @@ const Notes = () => {
       <li>MATH 237 - Calculus 3: <Link to={MATH237} target="_blank">summary</Link></li>
       <li>MATH 239 - Introduction to Combinatorics: <Link to={MATH239} target="_blank">summary</Link></li>
       <li>STAT 231 - Statistics: <Link to={STAT231} target="_blank">course notes</Link></li>
+      <li>PMATH 347 - Groups and Rings: <Link to={PMATH347} target="_blank">course notes</Link></li>
       {/* <li>MH 3701 (NTU) - Basic Optimization: Coming Soon</li>
       <li>CO 331 - Coding Theory: Coming Soon</li> */}
     </ul>
